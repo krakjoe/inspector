9ab198cb19bb065495e60e7091df8fc69980fce8
Joe Watkins <krakjoe@php.net>
2018-05-14 16:45:45 +0200

    ...

--- src/frame.c
+++ src/frame.c
@@ -219,11 +219,7 @@ ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(Frame_getPrevious_arginfo, 0, 0, IS_OBJE
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(Frame_getOperand_arginfo, 0, 0, 1)
-#if PHP_VERSION_ID >= 70200
 	ZEND_ARG_TYPE_INFO(0, which, IS_LONG, 0)
-#else
-	ZEND_ARG_TYPE_INFO(0, which, IS_LONG, NULL, 0)
-#endif
 ZEND_END_ARG_INFO()
 
 static zend_function_entry php_inspector_frame_methods[] = {
